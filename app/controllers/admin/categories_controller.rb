fffe3bb0eef6a1c7577d528b40e2334880e21f3b
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-05 03:46:58 +0000

    admin categories page shows up!

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,15 +1,39 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"], except: :denied
 
+  def denied
+    render plain: "get outa here!"
+  end
+
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
+    @categories = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
   def destroy
+    @categories = Category.find params[:id]
+    @categories.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
