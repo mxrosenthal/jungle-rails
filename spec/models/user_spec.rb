77fa154127a59746b3e50830c67e7f2e8da20722
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-09 23:19:12 +0000

    added validator to user.rb to check emails for uniqueness. added tests to user_spec to verify this uniqueness. also validates that first & last name, as well as email are required.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,31 @@ RSpec.describe User, type: :model do
       @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "1")
       expect(@user).to be_invalid
     end
-
     it "must be created with a password that matches the password_confirmation" do
       @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "123")
       expect(@user.password).to eq(@user.password_confirmation)
     end
-    
+    it "only allows unique emails (not case sensitive)" do
+      @user1 = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "123")
+      @user1.save
+      @user2 = User.new(first_name: "Barry", last_name: "Jerry", email: "A@A.com", password: "123", password_confirmation: "123")
+      expect(@user2).to be_invalid
+         
+    end
+    it "must be created with an email" do 
+      @user = User.new(first_name: "Max", last_name: "Xam", email: nil, password: "123", password_confirmation: "123")
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to contain_exactly("Email can't be blank")
+    end
+    it "must be created with a first name" do 
+      @user = User.new(first_name: nil, last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "123")
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to contain_exactly("First name can't be blank")
+    end
+    it "must be created with a last name" do 
+      @user = User.new(first_name: "Max", last_name: nil, email: "a@a.com", password: "123", password_confirmation: "123")
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to contain_exactly("Last name can't be blank")
+    end
   end
 end
