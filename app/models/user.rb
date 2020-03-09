f9b76d85e4bfdd9211ecbd2a70d16261c8dfa079
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-09 23:27:25 +0000

    added a min length validator to the password field and added a test to verify that a password below that length is invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,37 +5,41 @@ RSpec.describe User, type: :model do
     it "must be created with a password" do 
       @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: nil, password_confirmation: nil)
       expect(@user).to be_invalid
-      expect(@user.errors.full_messages).to contain_exactly("Password can't be blank")
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it "is not valid if passwords do not match" do 
-      @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "1")
+      @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123456", password_confirmation: "TerryFox")
       expect(@user).to be_invalid
     end
     it "must be created with a password that matches the password_confirmation" do
-      @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "123")
+      @user = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123456", password_confirmation: "123456")
       expect(@user.password).to eq(@user.password_confirmation)
     end
     it "only allows unique emails (not case sensitive)" do
-      @user1 = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "123")
+      @user1 = User.new(first_name: "Max", last_name: "Xam", email: "a@a.com", password: "123456", password_confirmation: "123456")
       @user1.save
-      @user2 = User.new(first_name: "Barry", last_name: "Jerry", email: "A@A.com", password: "123", password_confirmation: "123")
+      @user2 = User.new(first_name: "Barry", last_name: "Jerry", email: "A@A.com", password: "123456", password_confirmation: "123456")
       expect(@user2).to be_invalid
          
     end
     it "must be created with an email" do 
-      @user = User.new(first_name: "Max", last_name: "Xam", email: nil, password: "123", password_confirmation: "123")
+      @user = User.new(first_name: "Max", last_name: "Xam", email: nil, password: "123456", password_confirmation: "123456")
       expect(@user).to be_invalid
       expect(@user.errors.full_messages).to contain_exactly("Email can't be blank")
     end
     it "must be created with a first name" do 
-      @user = User.new(first_name: nil, last_name: "Xam", email: "a@a.com", password: "123", password_confirmation: "123")
+      @user = User.new(first_name: nil, last_name: "Xam", email: "a@a.com", password: "123456", password_confirmation: "123456")
       expect(@user).to be_invalid
       expect(@user.errors.full_messages).to contain_exactly("First name can't be blank")
     end
     it "must be created with a last name" do 
-      @user = User.new(first_name: "Max", last_name: nil, email: "a@a.com", password: "123", password_confirmation: "123")
+      @user = User.new(first_name: "Max", last_name: nil, email: "a@a.com", password: "123456", password_confirmation: "123456")
       expect(@user).to be_invalid
       expect(@user.errors.full_messages).to contain_exactly("Last name can't be blank")
+    end
+    it "Password must be at least 6 characters." do 
+      @user = User.new(first_name: "Max", last_name: "Xam", email: "nil@crud.com", password: "12345", password_confirmation: "12345")
+      expect(@user).to be_invalid
     end
   end
 end
